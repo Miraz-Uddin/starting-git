d9efde8208435849795391b82995acab149ebfcd
FazlyRabbi <rabbisam74@gmail.com>
2023-08-22 22:24:05 +0600

    add some sad text

--- src/component/Rabbi.jsx
+++ src/component/Rabbi.jsx
@@ -1,3 +1,14 @@
 export default function Rabbi() {
-	return <div>Rabbi</div>;
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "4rem",
+        fontSize: "3rem",
+        color: "black",
+      }}
+    >
+      My Teams account has gone with my content 🥺.{" "}
+    </div>
+  );
 }
